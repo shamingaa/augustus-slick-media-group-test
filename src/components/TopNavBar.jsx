23fc83ca38999ef39b820292b6a70166e4f9426a
shaminga6 <shamingaa@gmail.com>
2022-12-16 23:48:47 +0100

    build top navbar

---
+++ src/components/TopNavBar.jsx
@@ -0,0 +1,9 @@
+const TopNavBar = () => {
+    return ( 
+        <nav className="top-navbar">
+            <span className="logo">MyTestApp</span>
+        </nav>
+     );
+}
+ 
+export default TopNavBar;
