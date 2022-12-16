db4764d92161c8160a78ab5be6f6c65c159b234c
shaminga6 <shamingaa@gmail.com>
2022-12-17 00:56:09 +0100

    Build search input section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import SearchSection from "./components/SearchSection";
 import TopNavBar from "./components/TopNavBar";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="App">
       <TopNavBar />
       <Banner />
+      <div className="movies-section">
+        <SearchSection />
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchSection.jsx
@@ -0,0 +1,11 @@
+const SearchSection = () => {
+  return (
+    <div className="search-section">
+      <span id="search-label">Search</span>
+      <br />
+      <input type="text" id="search-input"/>
+    </div>
+  );
+};
+
+export default SearchSection;
