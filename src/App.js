2c07e7404c0e47d6845a2afbce322a6f79acd447
shaminga6 <shamingaa@gmail.com>
2022-12-17 00:38:06 +0100

    Build the banner section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import TopNavBar from './components/TopNavBar';
+import Banner from "./components/Banner";
+import TopNavBar from "./components/TopNavBar";
 
 function App() {
   return (
     <div className="App">
       <TopNavBar />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1 className="banner-title"> Watch something incredible.</h1>
+    </div>
+  );
+};
+
+export default Banner;
