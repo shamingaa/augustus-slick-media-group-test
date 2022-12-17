f60fcab0699d1a373354fc52780f5c8a47491304
shaminga6 <shamingaa@gmail.com>
2022-12-17 02:17:18 +0100

    Dynamically render searched movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import SearchedMovies from "./components/SearchedMovies";
@@ -6,80 +6,16 @@ import SearchSection from "./components/SearchSection";
 import TopNavBar from "./components/TopNavBar";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Iron Man",
-      Year: "2008",
-      imdbID: "tt0371746",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Man 3",
-      Year: "2013",
-      imdbID: "tt1300854",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Man 2",
-      Year: "2010",
-      imdbID: "tt1228705",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Iron Giant",
-      Year: "1999",
-      imdbID: "tt0129167",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYzBjZTNkMzQtZmNkOC00Yzk0LTljMjktZjk3YWVlZjY3NTk2XkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_SX300.jpg",
-    },
-    {
-      Title: "The Man in the Iron Mask",
-      Year: "1998",
-      imdbID: "tt0120744",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZjM2YzcxMmQtOTc2Mi00YjdhLWFlZjUtNmFmMDQzYzU2YTk5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Fist",
-      Year: "2017–2018",
-      imdbID: "tt3322310",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjI5Mjg1NDcyOV5BMl5BanBnXkFtZTgwMjAxOTQ5MTI@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Iron Lady",
-      Year: "2011",
-      imdbID: "tt1007029",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BODEzNDUyMDE3NF5BMl5BanBnXkFtZTcwMTgzOTg3Ng@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Sky",
-      Year: "2012",
-      imdbID: "tt1034314",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTM2MDg5MzgxNF5BMl5BanBnXkFtZTcwODUzNjMxOA@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Man with the Iron Fists",
-      Year: "2012",
-      imdbID: "tt1258972",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTg5ODI3ODkzOV5BMl5BanBnXkFtZTcwMTQxNjUwOA@@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([  ]);
+
+  useEffect(() => {
+    const getMovies = async (searchWord) => {
+      const response = await fetch(`https://omdbapi.com/?s=${searchWord}&apikey=c86d14a6`)
+      const data = await response.json()
+      setMovies(data)
+    }
+    getMovies("iron")
+  }, [])
 
   return (
     <div className="App">
@@ -87,7 +23,7 @@ function App() {
       <Banner />
       <div className="movies-section">
         <SearchSection />
-        <SearchedMovies movies={movies} />
+        <SearchedMovies moviesArr={movies} />
       </div>
     </div>
   );
--- src/components/SearchedMovies.jsx
+++ src/components/SearchedMovies.jsx
@@ -1,15 +1,19 @@
-const SearchedMovies = ({ movies }) => {
+const SearchedMovies = ({ moviesArr }) => {
+  const { Search } = moviesArr;
+
   return (
     <>
       <span id="search-label">Movies</span>
       <div className="movies-container">
         <div className="searched-movies">
-          {movies.map(({ Title, Poster }) => (
-            <div className="card">
-              <img src={Poster} alt=" movie poster" id="movie-poster" />
-              <div id="movie-name">{Title}</div>
-            </div>
-          ))}
+          {Search
+            ? Search.map(({ Title, Poster }) => (
+                <div className="card">
+                  <img src={Poster} alt=" movie poster" id="movie-poster" />
+                  <div id="movie-name">{Title}</div>
+                </div>
+              ))
+            : ""}
         </div>
       </div>
     </>
